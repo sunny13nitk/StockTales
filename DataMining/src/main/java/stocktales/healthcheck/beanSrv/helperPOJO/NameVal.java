7280ef3745c1ab5a63b584ff9bf1dc64b0eadea7
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2021-01-05 17:15:17 +0530

    commit

--- DataMining/src/main/java/stocktales/healthcheck/beanSrv/helperPOJO/NameVal.java
+++ DataMining/src/main/java/stocktales/healthcheck/beanSrv/helperPOJO/NameVal.java
@@ -1,5 +1,14 @@
 package stocktales.healthcheck.beanSrv.helperPOJO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NameVal
 {
 	private String name;
